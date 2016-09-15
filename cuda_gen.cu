9f026ba15ff25c3690aa09097e22f316e54e87af
Emanuel Schrade <schrade@kit.edu>
2016-09-15 19:37:55 +0200

    cuda: free memory

--- cuda_gen.cu
+++ cuda_gen.cu
@@ -22,6 +22,7 @@
 #include <algorithm>
 #include <cmath>
 #include <utility>
+#include <set>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323814
@@ -35,7 +36,7 @@ __global__ void initCurand(curandState *state, unsigned long long *seed)
 }
 
 //sample random numbers with geometric distribution
-  template <typename T>
+template <typename T>
 __global__ void sample(curandState *state, T *data, const size_t count, const T num, const double oneminusp)
 {
   size_t threadId = blockIdx.x * blockDim.x + threadIdx.x;
@@ -58,90 +59,52 @@ __global__ void sample(curandState *state, T *data, const size_t count, const T 
   state[threadId] = mState;
 }
 
-#if 0
-//naive way of removing excessive indices
-  template <typename T>
-__global__ void naive(curandState *state, T *data, const T *split, const T *to_remove)
+//set elements to MAX to mark for removal
+template <typename T>
+__global__ void markRemove(T *data, const size_t count, T *remove, const size_t remove_count)
 {
   size_t threadId = blockIdx.x * blockDim.x + threadIdx.x;
-  T idx = split[threadId];
-  T num = split[threadId+1]-split[threadId];
-  curandState mState = state[threadId];
+  if(threadId < remove_count)
+    data[remove[threadId]] = T(-1);
+}
 
-  for(unsigned int i = 0; i < to_remove[threadId]; i++)
+//XXX Not working in all cases
+#if 0
+//replace k elements with k last ones
+template <typename T>
+__global__ void replace(T *data, const size_t count, T *remove, const size_t remove_count)
+{
+  size_t threadId = blockIdx.x * blockDim.x + threadIdx.x;
+  T idx = (T)-1;
+  if(threadId < remove_count)
+    idx = remove[threadId];
+
+  if(idx != (T)-1 && data[idx] == (T)-1 && data[count-threadId-1] != (T)-1)
   {
-    //pick a random element and remove it if it wasn't removed before
-    T r = curand_uniform(&mState)*num;
-    if(data[idx+r] == (T)-1)
-      --i;
-    data[idx+r] = (T)-1;
+    data[idx] = data[count-threadId-1];
+    remove[threadId] = (T)-1;
   }
-  state[threadId] = mState;
+  if(idx == count-threadId-1)
+    remove[threadId] = (T)-1;
+  //otherwise replace in next iteration
 }
 #endif
 
-//Vitter's Algorithm A (base case of Algorithm D)
-  template <typename T>
-__global__ void algorithmA(curandState *state, T *data, const T *split, const T *to_remove)
-{
-  size_t threadId = blockIdx.x * blockDim.x + threadIdx.x;
-  T idx = split[threadId];
-  T num = split[threadId+1]-split[threadId];
-  curandState mState = state[threadId];
-
-  float top = num - to_remove[threadId];
-  float Nreal = num;
-  float r = curand_uniform(&mState);  
-  float quot = top/Nreal;
-
-  for(unsigned int i = 0; i < num; i++, idx++)
-  {
-    if(quot <= r)
-    {
-      r = curand_uniform(&mState);
-      data[idx+1] = (T)-1;
-      quot = 1;
-    } else
-      top--;
-    Nreal--;
-    quot *= top/Nreal;
-  }
-  state[threadId] = mState;
-}
-
-//functor for filtering elements less or equal a constant
+//functor for filtering elements less than a constant
 template <typename T>
-struct less_equal_functor
+struct less_functor
 {
   T max_idx;
-  less_equal_functor(const T _max_idx)
+  less_functor(const T _max_idx)
     : max_idx(_max_idx)
   { }
 
   __host__ __device__
     bool operator()(T x)
     {
-      return x <= max_idx;
+      return x < max_idx;
     }
 };
-
-#if 0
-//functor for filtering elements greater than a constant
-template <typename T>
-struct greater_than_functor
-{
-  T max_idx;
-  greater_than_functor(const T _max_idx)
-    : max_idx(_max_idx)
-  { }
-
-  __host__ __device__
-    bool operator()(T x)
-    {
-      return x > max_idx;
-    }
-};
-#endif
 
 //initialize prng only once (seed can be set nevertheless)
 template <typename T>
@@ -160,7 +123,6 @@ class cuda_generator
     const T num_threads = 1<<16;
     const T num_threads_per_group = 1<<3;
     const T num_groups = num_threads / num_threads_per_group;
-    T *device_split, *device_to_remove, *split, *to_remove;
 
     cuda_generator()
     {
@@ -169,24 +131,13 @@ class cuda_generator
       cudaMalloc((void**)&seeds_device, num_threads * sizeof(unsigned long long));
       seeds = new unsigned long long[num_threads];
       device_data = 0;
-      split = new T[num_threads+1];
-      to_remove = new T[num_threads];
-
-      //remove excessive elements per processor
-      cudaMalloc((void**)&device_split, sizeof(T)*(num_threads+1));
-      cudaMalloc((void**)&device_to_remove, sizeof(T)*num_threads);
     }
 
     ~cuda_generator()
     {
-      if(split)
-        delete [] split;
-      if(to_remove)
-        delete [] to_remove;
-      cudaFree((void**)&device_split);
-      cudaFree((void**)&device_to_remove);
       cudaFree((void**)&state);
       cudaFree((void**)&seeds_device);
+      delete [] seeds;
     }
 
 
@@ -198,81 +149,81 @@ class cuda_generator
       return _instance;
     }
 
-    static void sampleR(T n, T N, T j, T k, T *startIdx, T *numSamples, StochasticLib1 stoc = StochasticLib1(0), T off = 0)
-    {
-      if(k == j+1)
-      {
-        numSamples[j] = n;
-        startIdx[j] = N+off;
-        return;
-      }
-      T N2 = N/2;
-      T x = stoc.Hypergeometric(n, N2, N);
-      sampleR(x, N2, j, (j+k)/2, startIdx, numSamples, stoc, off);
-      sampleR(n-x, N-N2, (j+k)/2, k, startIdx, numSamples, stoc, off+N2);
-    }
-
     template <typename It>
-      void generate_block(It dest, size_t size, size_t k, size_t universe, double p,
-          unsigned long long seed = 0)
+    void generate_block(It dest, size_t size, size_t k, size_t universe, double p,
+      unsigned long long seed = 0)
+    {
+      using value_type = typename std::iterator_traits<It>::value_type;
+      assert(p > 0 && p < 1);
+
+      if (seed == 0)
       {
-        //fprintf(stderr, "%d: universe=%Iu\n", __LINE__, universe);
-        using value_type = typename std::iterator_traits<It>::value_type;
-        assert(p > 0 && p < 1);
-
-        if (seed == 0)
-        {
-          seed = std::random_device{}();
-        }
-
-        //allocate memory on GPU if blocksize changed
-        if(N != size)
-        {
-          if(device_data)
-            cudaFree(device_data);
-          cudaMalloc((void**)&device_data, sizeof(T)*size);
-          N = size;
-        }
-        for(int i = 0; i < num_threads; i++)
-          seeds[i] = seed+i;
-        cudaMemcpy(seeds_device, seeds, num_threads*sizeof(unsigned long long), cudaMemcpyHostToDevice);
-        //initialize prng-states
-        initCurand<<<num_groups, num_threads_per_group>>>(state, seeds_device);
-        //fill array with geometrically distributed random numbers
-        T samples_per_thread = max((T)ceil(N/(double)num_threads), (T)1);
-        T num_indices = 0;
-        timer t;
-        do {
-          sample<<<num_groups, num_threads_per_group>>>(state, device_data, N, samples_per_thread, 1.0-p);
-          //prefix-sum for calculating array-indices
-          thrust::inclusive_scan(thrust::device, device_data, device_data+N, device_data);
-          //count_if is faster than the other two.
-          num_indices = thrust::count_if(thrust::device, device_data, device_data+N, less_equal_functor<T>((T)universe));
-          //num_indices = thrust::find_if(thrust::device, device_data, device_data+N, less_equal_functor<T>((T)universe)) - device_data;
-          //num_indices = thrust::copy_if(thrust::device, device_data, device_data+N, device_data, less_equal_functor<T>((T)universe)) - device_data;
-        } while(num_indices < k);
-        cudaDeviceSynchronize();
-
-        //divide array using hypergeometric deviates (one subarray per processor)
-        sampleR(num_indices-k, num_indices, 0, num_threads, split+1, to_remove);
-        split[0] = 0;
-        //copy start- and end-indices of subarrays as well as the number
-        //of elements to be sampled from these subarrays to GPU memory
-        cudaMemcpy(device_split, split, sizeof(T)*(num_threads+1), cudaMemcpyHostToDevice);
-        cudaMemcpy(device_to_remove, to_remove, sizeof(T)*num_threads, cudaMemcpyHostToDevice);
-
-        //sample elements (elements are removed by setting the index to some value larger
-        //than the universe size
-        //naive<<<num_groups, num_threads_per_group>>>(state, device_data, device_split, device_to_remove);
-        algorithmA<<<num_groups/4, num_threads_per_group*4>>>(state, device_data, device_split, device_to_remove);
-
-        //cudaDeviceSynchronize();
-
-        //compaction
-        thrust::copy_if(thrust::device, device_data, device_data+num_indices, device_data, less_equal_functor<T>(universe));
-        //read back data
-        //cudaMemcpy(&dest[0], device_data, n*sizeof(T), cudaMemcpyDeviceToHost);
+        seed = std::random_device{}();
       }
+
+      //allocate memory on GPU if blocksize changed
+      if(N != size)
+      {
+        if(device_data)
+          cudaFree(device_data);
+        cudaMalloc((void**)&device_data, sizeof(T)*size);
+        N = size;
+      }
+      for(int i = 0; i < num_threads; i++)
+        seeds[i] = seed+i;
+      cudaMemcpy(seeds_device, seeds, num_threads*sizeof(unsigned long long), cudaMemcpyHostToDevice);
+      //initialize prng-states
+      initCurand<<<num_groups, num_threads_per_group>>>(state, seeds_device);
+      //fill array with geometrically distributed random numbers
+      T samples_per_thread = max((T)ceil(N/(double)num_threads), (T)1);
+      T num_indices = 0;
+      timer t;
+      do {
+        sample<<<num_groups, num_threads_per_group>>>(state, device_data, N, samples_per_thread, 1.0-p);
+        //prefix-sum for calculating array-indices
+        thrust::inclusive_scan(thrust::device, device_data, device_data+N, device_data);
+        //count_if is faster than the other two.
+        num_indices = thrust::count_if(thrust::device, device_data, device_data+N, less_functor<T>((T)universe));
+        //num_indices = thrust::find_if(thrust::device, device_data, device_data+N, less_functor<T>((T)universe)) - device_data;
+        //num_indices = thrust::copy_if(thrust::device, device_data, device_data+N, device_data, less_functor<T>((T)universe)) - device_data;
+      } while(num_indices < k);
+
+      //sample num_indices - k elements to remove
+      std::mt19937 gen(seed);
+      std::uniform_int_distribution<T> dist(0, num_indices-1);
+      std::set<T> remove;
+
+      while(remove.size() < num_indices - k)
+        remove.insert(dist(gen));
+      //copy indices to array and copy to GPU
+      T *removeArray = new T[num_indices - k];
+      int idx = 0;
+      for(auto it = remove.begin(); it != remove.end(); it++)
+        removeArray[idx++] = *it;
+      T *device_removeArray;
+      cudaMalloc((void**)&device_removeArray, sizeof(T) * (num_indices-k));
+      cudaMemcpyAsync(device_removeArray, removeArray, sizeof(T) * (num_indices-k), cudaMemcpyHostToDevice);
+
+      markRemove<<<(num_indices-k)/num_threads_per_group*4, num_threads_per_group*4>>>(device_data, num_indices, device_removeArray, num_indices-k);
+      #if 1
+      //compaction
+      int removed = thrust::copy_if(thrust::device, device_data, device_data+num_indices, device_data, less_functor<T>(universe)) - device_data;
+      #else
+      //copy elements and count collisions, restart until no collisions left
+      remove_remaining = num_indices-k;
+      while(remove_remaining > 0)
+      {
+        replace<<<remove_remaining, num_threads_per_group>>>(device_data, k+remove_remaining, device_removeArray, num_indices-k);
+        remove_remaining = thrust::copy_if(thrust::device, device_removeArray, device_removeArray + remove_remaining, device_removeArray, less_functor<T>((T)universe))-device_removeArray; 
+      }
+
+      cudaFree(&device_removeArray);
+      #endif
+      cudaFree(&device_removeArray);
+      delete [] removeArray;
+      //read back data
+      //cudaMemcpy(&dest[0], device_data, n*sizeof(T), cudaMemcpyDeviceToHost);
+    }
 };
 template <typename T> cuda_generator<T> *cuda_generator<T>::_instance = 0;
 
@@ -308,7 +259,7 @@ static std::pair<double, size_t> calc_params(size_t universe, size_t k /* sample
 
 int main(int argc, char **argv)
 {
-  cudaSetDevice(1);
+  cudaSetDevice(3);
   size_t num_threads = 1;
   arg_parser args(argc, argv);
   size_t universe = args.get<size_t>("n", 1<<30);
@@ -326,23 +277,17 @@ int main(int argc, char **argv)
   #else
   std::vector<unsigned int> vec(ssize);
   #endif
-  int version = 0;
-  curandGetVersion(&version);
-  fprintf(stderr, "%d", version);
   statistics mt_stats;
 
   // warmup
   // k = number of indices (output), p = probability, universe = maximum index size
   // ssize = size of index array
   cuda_gen::generate_block(vec.begin(), ssize, k, universe, p);
-  //std::cout << "Running warmup (" << k << " samples)" << std::endl;
 
   std::stringstream extra_stream;
   extra_stream << " k=" << k << " b=" << ssize
     << " p=" << p << " N=" << universe;
   auto extra = extra_stream.str();
-
-  //std::cout << "Running measurements..." << std::endl;
 
   // Measure
   cudaDeviceSynchronize();
